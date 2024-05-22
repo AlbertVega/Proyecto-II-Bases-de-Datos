ed3aa4047bbc5ba8e9f40ee92c544e716a69e5ce
Albert Vega camacho <85046754+AlbertVega@users.noreply.github.com>
2024-05-21 21:47:28 -0600

    FrontEnd patient-register v1

--- HospiTEC/hospitec.client/src/app/app-routing.module.ts
+++ HospiTEC/hospitec.client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatientRegisterComponent } from './components/patient-register/patient-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'patient-reg', component: PatientRegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ HospiTEC/hospitec.client/src/app/components/patient-register/patient-register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-register',
+  templateUrl: './patient-register.component.html',
+  styleUrl: './patient-register.component.css'
+})
+export class PatientRegisterComponent {
+
+}
