30dfa4c5be63c24d3f1ee2143eb797b6089364a6
DanielCob <83792193+DanielCob@users.noreply.github.com>
2024-06-03 11:57:37 -0600

    Reserva cama

--- HospiTEC/hospitec.client/src/app/app-routing.module.ts
+++ HospiTEC/hospitec.client/src/app/app-routing.module.ts
@@ -7,13 +7,16 @@ import { HistorialViewComponent } from './components/patient-view/historial-view
 import { DoctorLoginComponent } from './components/doctor-login/doctor-login.component';
 import { DoctorViewComponent } from './components/doctor-view/doctor-view.component';
 import { AddPatientComponent } from './components/doctor-view/add-patient/add-patient.component';
+import { GestionReservacionComponent } from './components/patient-view/gestion-reservacion/gestion-reservacion.component';
 
 const routes: Routes = [
   { path: 'patient-reg', component: PatientRegisterComponent },
   { path: 'patient-log', component: PatientLoginComponent },
   {
     path: 'patient-view', component: PatientViewComponent,
-    children: [{path:'historial-view',component:HistorialViewComponent}
+    children: [
+        { path: 'historial-view', component: HistorialViewComponent },
+        { path: 'gestion-reservacion', component: GestionReservacionComponent}
       ]
   },
   { path: 'doctor-login', component: DoctorLoginComponent },
