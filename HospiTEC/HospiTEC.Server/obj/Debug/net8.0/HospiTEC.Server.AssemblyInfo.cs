889da025fb5dfc06fa9622ba9d2158cbd9df03e0
Tavo121 <galvarado13@estudiantec.cr>
2024-06-04 16:10:15 -0600

    Server deploy from doctor-view

--- HospiTEC/HospiTEC.Server/obj/Debug/net8.0/HospiTEC.Server.AssemblyInfo.cs
+++ HospiTEC/HospiTEC.Server/obj/Debug/net8.0/HospiTEC.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospiTEC.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f470c53bcc2c8ca84514cdbc013159b563dec64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e056e044e9a9184c1d1dc1998833f11415bae80")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospiTEC.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospiTEC.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
